b1b9a6712eefa4913b1d847b8f268c65667a8348
Jiahong Zheng <KZheng@walmart.com>
2020-01-19 08:00:32 -0800

    add more css to google map

--- src/components/common/google-map.jsx
+++ src/components/common/google-map.jsx
@@ -1,38 +1,64 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
+import { Card } from "semantic-ui-react";
+
+const CardExampleLinkCard = ({ header, meta, description }) => (
+  <Card header={header} meta={meta} description={description} />
+);
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class SimpleMap extends Component {
+const MAIN_CLASS = "GoogleMap";
+
+class GoogleMap extends Component {
   static defaultProps = {
     center: {
       lat: 59.95,
       lng: 30.33
     },
+    coordinates: {
+        lat: 59.95,
+        lng: 30.33
+      },
     zoom: 11
   };
 
   render() {
-      console.log('this.props', this.props)
     const {
-      center: { lat, lng }
+      center: { lat, lng },
+      city,
+      state,
+      street,
+      postalCode,
+      coordinates
     } = this.props;
+
+    const description = `${street}, ${city}, ${state} ${postalCode}`;
+    const meta = `lag: ${coordinates.lag} ${coordinates.lng}`;
     return (
       // Important! Always set the container height explicitly
-      <div>
-      <div></div>
-      <div style={{ height: "100vh", width: "100%" }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyBf6otTGdU41Hyu44Ed3l676m0bN3eLSH8" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent lat={lat} lng={lng} text="My Marker" />
-        </GoogleMapReact>
-      </div>
+      <div className={`${MAIN_CLASS}`}>
+        <div className={`${MAIN_CLASS}-company-info`}>
+          <CardExampleLinkCard
+            header={city}
+            meta={meta}
+            description={description}
+          />
+        </div>
+        <div className={`${MAIN_CLASS}-google-map-info`} style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyBf6otTGdU41Hyu44Ed3l676m0bN3eLSH8"
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent lat={lat} lng={lng} text="My Marker" />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
 }
 
-export default SimpleMap;
+export default GoogleMap;
