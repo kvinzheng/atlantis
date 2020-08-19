3ad525e8f23e729088ab1a166c7247a3dc3ca96f
Zidian Lyu <zlu@ucdavis.edu>
2020-08-19 12:59:53 -0700

    update the backend API data endpoint.

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://atlantis-264506.appspot.com/api/"
+  baseURL: 'https://atlantis-legend.herokuapp.com/api/',
 });
